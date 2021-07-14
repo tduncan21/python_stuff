c8cdf02d19f667fa52c7cbbf8396c2a305882fd2
tduncan94 <83376555+tduncan94@users.noreply.github.com>
2021-07-14 10:57:47 -0500

    Handle non alpha characters

--- 100_Days/Day_8/caesar_cipher.py
+++ 100_Days/Day_8/caesar_cipher.py
@@ -1,9 +1,13 @@
 from caesar_cipher_alphabet import get_alphabet
+from caesar_cipher_art import logo
 
 
 def encrypt(user_message, shift):
     encrypted_message = ""
     for letter in user_message:
+        if letter not in alphabet:
+            encrypted_message += letter
+            continue
         shifted_letter_index = 0
         index = alphabet.index(letter)
         index += shift
@@ -18,6 +22,9 @@ def encrypt(user_message, shift):
 def decrypt(user_message, shift):
     encrypted_message = ""
     for letter in user_message:
+        if letter not in alphabet:
+            encrypted_message += letter
+            continue
         shifted_letter_index = 0
         index = alphabet.index(letter)
         index -= shift
@@ -40,9 +47,14 @@ def continue_cipher():
 
 ciphering = True
 alphabet = get_alphabet()
+print(logo)
 
 while ciphering:
     user_input = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n" )
+    if user_input != "encode" and user_input != "decode":
+        print("Invalid input, try again")
+        continue
+
     user_message = input("Type your message:\n")
     shift = int(input("Type the shift number:\n"))
 
